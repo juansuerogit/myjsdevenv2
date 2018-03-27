97ca4747b92ca8e9444cd1ce40b888b8d9d84499
metadojoai <juanblockchain.suero@gmail.com>
2018-03-27 14:08:43 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 readme
+
+echo "# myjsdevenv2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/juansuerogit/myjsdevenv2.git
+git push -u origin master
---
+++ buildscripts/srcServer.js
@@ -0,0 +1,23 @@
+
+
+import express from 'express'
+import path from 'path'
+import open from 'open'
+
+const port = 3000
+const app = express()
+
+app.get('/', function(req, res){
+
+    res.sendFile(path.join(__dirname, '../src/index.html'))
+
+})
+
+app.listen(port, function(err) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('Listening at http://localhost:' + port)
+        open('http://localhost:' + port)
+    }
+})
